592aa317b812df39f5ce3c07f6473b30e82f56c1
Denis O'Meally <omeally@gmail.com>
2024-03-05 12:12:16 -0700

    Refactor run_cellbender function to improve readability and add support for additional arguments

--- R/run_cellbender.R
+++ R/run_cellbender.R
@@ -1,78 +1,59 @@
-#' @title Run CellBender
+
+#' Run CellBender analysis on a Cell Ranger run folder
 #'
-#' @description Runs CellBender using the 10x h5 file as input
-#' @param rawFeatureMatrix The path to 10x's raw_feature_bc_matrix.h5
-#' @param expectedCells Passed to CellBender --expected-cells
-#' @param totalDropletsIncluded Passed to CellBender --total-droplets-included
-#' @param fpr Passed to CellBender --fpr
-#' @param epochs Passed to CellBender --epochs
+#' This function runs the CellBender analysis on a Cell Ranger run folder.
 #'
+#' @param cellranger_run_folder The path to the Cell Ranger run folder.
+#' @param additional_args Additional arguments to be passed to the CellBender command.
+#'
+#' @return The path to the CellBender output file.
+#'
+#' @examples
+#' if_interactive({
+#' run_cellbender("/path/to/cellranger_run_folder")
+#' run_cellbender("/path/to/cellranger_run_folder", additional_args = "--param1 value1 --param2 value2")
+#' })
+#' 
 #' @export
-run_cellbender <- function(run_folder, expectedCells = 5000, totalDropletsIncluded = 20000, fpr = 0.01, epochs = 150) {
-  # Check if CellBender is in the PATH
-  cellBenderPath <- Sys.which("cellbender")
-  if (nzchar(cellBenderPath)) {
-    print(paste("Running CellBender from:", cellBenderPath))
-  } else {
-    stop("CellBender is not found in the PATH. Please ensure CellBender is installed and accessible.")
-  }
-run_folder<-"/scratch/domeally/DCD.tienhoven_scRNAseq.2024/cellranger_out/10xv2/nfcore-scrnaseq-v2.5.1-refdata-gex-GRCh38-2020-A/cellranger/count/HPAP-019"
-  # Define the output file path
-  outH5File <- tempfile(fileext = '.h5')
-  rawFeatureMatrix <- paste0(run_folder, "outs/raw_feature_bc_matrix.h5")
+run_cellbender <- function(cellranger_run_folder, additional_args = "") {
 
+    sample_id <- basename(cellranger_run_folder)
+    run_path <- glue::glue("{analysis_cache}/cellbender_out/{sample_id}")
 
-  # Construct CellBender command
-  cmd <- sprintf("cellbender remove-background --input %s --output %s --expected-cells %d --total-droplets-included %d --fpr %f --epochs %d",
-                 shQuote(rawFeatureMatrix),
-                 shQuote(outH5File),
-                 expectedCells,
-                 totalDropletsIncluded,
-                 fpr,
-                 epochs)
+    raw_feature_matrix <- glue::glue("{cellranger_run_folder}/outs/raw_feature_bc_matrix.h5")
+    out_file_h5 <- glue::glue("{run_path}/{sample_id}_cellbender.h5")
 
-  # Check if CUDA is available and append --cuda if truesbatch --chdir=$(pwd)
-  cudaCheckCmd <- "/usr/bin/python3 -c 'import torch; print(\"--cuda\" if torch.cuda.is_available() else \"\")'"
-  cudaAvailable <- system(cudaCheckCmd, intern = TRUE)
-  if (cudaAvailable == "--cuda") {
-    cmd <- paste(cmd, "--cuda")
-  }
+    cellbender_exe <- "/packages/singularity/3.11.5/bin/singularity exec --nv $SINGULARITY_CACHEDIR/cellbender_latest.sif cellbender"
 
-  # Execute the CellBender command
-  system(cmd)
+    dir.create(run_path, recursive = TRUE, showWarnings = FALSE)
+    
+    # Check if a successful cellbender run exists
+    if (file.exists(glue::glue("{run_path}/{sample_id}_cellbender.h5"))) {
+        print(glue::glue("Found existing cellbender run, skipping run for {sample_id}..."))
+        return(glue::glue("{run_path}/{sample_id}_cellbender.h5"))
+    } else if (!file.exists(glue::glue("{run_path}/run_cellbender.sh"))) {
+        # Create a sbatch script
+        script_content <- glue::glue("#!/bin/bash
+#SBATCH --job-name={sample_id}_cellbender
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --mem=60G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --chdir={run_path}
+#SBATCH -e {run_path}/slurm-%j.err
+#SBATCH -o {run_path}/slurm-%j.out
 
-  # Check for the existence of the output file
-  outputFiltered <- sub(pattern = '.h5$', replacement = '_filtered.h5', outH5File)
-  if (!file.exists(outputFiltered)) {
-    stop(paste0('Missing file: ', outputFiltered))
-  }
-
-  # Load the filtered data into a Seurat object
-  seuatRawData <- Seurat::Read10X_h5(filename = outputFiltered, use.names = TRUE)
-  print(paste0('Cells in CellBender filtered matrix: ', ncol(seuatRawData)))
-
-  return(seuatRawData)
+{cellbender_exe} remove-background --cuda --cpu-threads 8 --input {raw_feature_matrix} --output {out_file_h5} \\
+        {additional_args}
+")
+        cat(script_content, file = glue::glue("{run_path}/run_cellbender.sh"))
+        # Submit to the cluster & return the path of the run script
+        system(glue::glue("sbatch {run_path}/run_cellbender.sh && sleep 0.1"), wait = FALSE)
+        return(glue::glue("{run_path}/run_cellbender.sh"))
+    } else {
+        print(glue::glue("The CellBender run for \"{sample_id}\" has already been submitted. Check the SLURM job queue or navigate to {run_path} and submit the `run_cellbender.sh` script to resume the run."))
+        return(glue::glue("{run_path}/run_cellbender.sh"))
+    }
 }
-
-
-srun -n 6 -N 1-1 --mem=60G --gres=gpu:1 -p gpu --time=4:00:00 --pty bash
-
-library(crew)
-library(hprcc)
-controller <- 
-
-create_controller(
-  "gpu",
-  slurm_cpus = 6,
-  slurm_mem_gigabytes = 60,
-  slurm_walltime_minutes = 360,
-  slurm_workers = 35,
-  slurm_partition = "gpu",
-  slurm_log_dir = "logs"
-)
-
-crew_controller_local(
-  name = "example",
-  workers = 2,
-  seconds_idle = 10
-)
