b9b712ba29ce678b9bc5169dd7be8777cd1fc168
Denis O'Meally <omeally@gmail.com>
2024-03-07 22:24:55 -0700

    Refactor make_seurat_tosti_etal function to download and process data

--- R/make_seurat_tosti_etal.R
+++ R/make_seurat_tosti_etal.R
@@ -3,26 +3,56 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param download_tosti_etal_files
 #' @return
 #' @author Denis O'Meally
 #' @export
-make_seurat_tosti_etal <- function(download_tosti_etal_files) {
+make_seurat_tosti_etal <- function() {
 
-  #https://cellbrowser.readthedocs.io/en/master/load.html
-    mat <- data.table::fread(download_tosti_etal_files[1])
+    base_url <- "http://singlecell.charite.de/cellbrowser/pancreas/Adult_Pancreas/"
+    files_to_download <- c("exprMatrix.tsv.gz", "meta.tsv", "Seurat_umap.coords.tsv.gz")
+
+    download_tosti_etal_files <- lapply(files_to_download, function(file) {
+    dest_file_path <- file.path(analysis_cache, "data/tosti_etal", file)
+    dir.create(dirname(dest_file_path), showWarnings = FALSE, recursive = TRUE)
+    
+    # Check if this specific file does not exist before downloading
+    if (!file.exists(dest_file_path)) {
+        url <- paste0(base_url, file)
+        download.file(url, destfile = dest_file_path, mode = "wb")
+    }
+    return(dest_file_path) # Return the destination path for each file
+    })
+
+    # Convert the list of paths to a character vector
+    download_tosti_etal_files <- unlist(download_tosti_etal_files)
+
+    #https://cellbrowser.readthedocs.io/en/master/load.html
     meta <- read.table(download_tosti_etal_files[2], header=T, sep="\t", as.is=T, row.names=1)
     umap <- data.table::fread(download_tosti_etal_files[3])
     umap_coords <- as.matrix(umap[, -1])
     rownames(umap_coords) <- umap$V1
-    genes = mat[,1][[1]]
-    genes = gsub(".+[|]", "", genes)
-    mat = data.frame(mat[,-1], row.names=genes)
-    so <- CreateSeuratObject(counts = mat, project = "adultPancreas", meta.data=meta)
-    so[["umap"]] <- CreateDimReducObject(embeddings = umap_coords, key = "UMAP_", name = "umap")
+    colnames(umap_coords) <- c("UMAP_1", "UMAP_2")
 
-    so_path <- (paste0(analysis_cache, "/data/seurat_tosti_etal_", Sys.Date(), ".qs"))
+    # Read expression matrix and convert to sparse matrix
+    exprMatrix <- data.table::fread(download_tosti_etal_files[1], header = TRUE, sep = "\t", data.table = FALSE)
+    rownames(exprMatrix) <- gsub(".+[|]", "", exprMatrix[, 1])
+    exprMatrix <- exprMatrix[, -1]
+    sparse_mat <- as(Matrix::Matrix(as.matrix(exprMatrix), sparse = TRUE), "dgCMatrix")
+
+    # Create Seurat object
+    so <- Seurat::CreateSeuratObject(counts = sparse_mat, project = "tosti_etal", meta.data = meta) 
+    # Normalize the data
+    so <- Seurat::NormalizeData(so)
+    # Add the UMAP coordinates to the Seurat object
+    so[["umap"]] <- Seurat::CreateDimReducObject(embeddings = umap_coords, key = "UMAP_")
+
+    # make a UMAP plot
+    plot <- Seurat::DimPlot(so, group.by = "Cluster", reduction = "umap")
+    ggsave(glue::glue("{analysis_cache}/data/seurat_tosti_etal.png", plot = umap_plot, width = 12, height = 8, dpi = 300))
+
+    so_path <- (glue::glue("{analysis_cache}/data/seurat_tosti_etal.qs"))
+
     qs::qsave(so, so_path)
+    
     return(so_path)
-
 }
