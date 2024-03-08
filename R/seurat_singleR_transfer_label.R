ff35fceea615d26c548c1900a5ec6b9acdb0d5ae
Denis O'Meally <omeally@gmail.com>
2024-03-08 12:02:55 -0700

    Add functions for annotating and transferring cell types using SingleR

--- R/seurat_singleR_transfer_label.R
+++ R/seurat_singleR_transfer_label.R
@@ -1,11 +1,11 @@
-#' Transfer Cell Type Annotation from Reference to Test Seurat Object
+#' Transfer cell labels from reference to query Seurat Object
 #'
 #' This function applies cell type annotations from a reference Seurat object
-#' to a test Seurat object using SingleR. It supports optional normalization
+#' to a query Seurat object using SingleR. It supports optional normalization
 #' of the reference object and uses differential expression methods to assist
 #' with the annotation transfer.
 #'
-#' @param test_seurat_object a seurat object or path to the QS file for the test Seurat object.
+#' @param query_seurat_object a seurat object or path to the QS file for the query Seurat object.
 #' @param ref_seurat_object a seurat object or path to the QS file for the reference Seurat object.
 #' @param cell_type_col The name of the metadata column in the reference Seurat object
 #'        that contains cell type annotations.
@@ -14,23 +14,23 @@
 #'        Default is "wilcox".
 #'
 #' @return The path to the CSV file containing the transferred cell type annotations
-#'         for the test Seurat object.
+#'         for the query Seurat object.
 #'
 #' @details The function first performs initial quality control (using an assumed
-#'          `initialQC` function) and normalization on the test Seurat object. It
+#'          `initialQC` function) and normalization on the query Seurat object. It
 #'          optionally performs normalization on the reference Seurat object as well.
 #'          SingleR is then used to transfer cell type annotations from the reference
-#'          to the test dataset. The function is designed to work in a high-performance
+#'          to the query dataset. The function is designed to work in a high-performance
 #'          computing environment, leveraging parallel processing capabilities.
 #'
 #' @examples
-#' test_seurat_object <- "path/to/test_seurat_object.qs"
+#' query_seurat_object <- "path/to/query_seurat_object.qs"
 #' ref_seurat_object <- "path/to/reference_seurat_object.qs"
 #' cell_type_col <- "cell_type"
 #' 
 #' # Transfer cell type annotation without normalizing the reference
 #' result_path <- seurat_transfer_cell_type_annotation(
-#'   test_seurat_object,
+#'   query_seurat_object,
 #'   ref_seurat_object
 #'   cell_type_col,
 #'   norm_ref = FALSE,
@@ -47,21 +47,12 @@
 #' @importFrom tibble rownames_to_column as_tibble
 #' @importFrom glue glue
 #' @export
-seurat_transfer_cell_type_annotation <- function(test_seurat_object, ref_seurat_object, cell_type_col, de_method = "wilcox") {
+seurat_singleR_transfer_label <- function(query_seurat_object, ref_seurat_object, cell_type_col, de_method = "wilcox") {
+    query_seurat_object <- load_seurat(query_seurat_object) |> initialQC()
+    ref_seurat_object <- load_seurat(ref_seurat_object) |> initialQC()
 
-    if (!inherits(test_seurat_object, "Seurat")) {
-        test_seurat_object <- qs::qread(test_seurat_object)
-    }
-    if (!inherits(ref_seurat_object, "Seurat")) {
-        ref_seurat_object <- qs::qread(ref_seurat_object)
-    }
-
-    test_seurat_object <- test_seurat_object |> initialQC()
-    
-    ref_seurat_object <- ref_seurat_object |> initialQC() 
-
-    if (!isNormalized(test_seurat_object)) {
-        test_seurat_object <- Seurat::NormalizeData(test_seurat_object)
+    if (!isNormalized(query_seurat_object)) {
+        query_seurat_object <- Seurat::NormalizeData(query_seurat_object)
     }
 
     if (!isNormalized(ref_seurat_object)) {
@@ -78,7 +69,7 @@ seurat_transfer_cell_type_annotation <- function(test_seurat_object, ref_seurat_
 
     # Run SingleR
     cell_labels <- SingleR::SingleR(
-        test = Seurat::GetAssayData(test_seurat_object, assay = "RNA"),
+        test = Seurat::GetAssayData(query_seurat_object, assay = "RNA"),
         ref =  Seurat::GetAssayData(ref_seurat_object, assay = "RNA"),
         labels = ref_seurat_object@meta.data[[cell_type_col]],
         de.method = de_method,
@@ -94,7 +85,7 @@ seurat_transfer_cell_type_annotation <- function(test_seurat_object, ref_seurat_
         tibble::rownames_to_column(var = "cell") |> 
         tibble::as_tibble()
 
-    sample_id <- test_seurat_object@meta.data$orig.ident[1]
+    sample_id <- query_seurat_object@meta.data$orig.ident[1]
     ref_id <- ref_seurat_object@project.name
     
     cell_type_table_path <- glue::glue("{analysis_cache}/cell_type_out/{sample_id}_{ref_id}_cell_type.csv")
