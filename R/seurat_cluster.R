a5794d91725036b711b446e0fd16b81f39725d65
Denis O'Meally <omeally@gmail.com>
2024-03-16 23:24:44 -0700

    Refactor seurat_cluster_ari function to improve readability and add plot option

--- R/seurat_cluster.R
+++ R/seurat_cluster.R
@@ -1,35 +1,43 @@
 #' Cluster cells using Seurat package
 #'
-#' This function clusters cells using the Seurat package. It normalizes the data, finds variable features, scales the data, runs PCA, finds neighbors, and finally clusters the cells. It returns a metadata table with the cell names and their corresponding clusters.
-#'
-#' @param seurat_object A Seurat object (or its path) containing the single-cell RNA-seq data.
+#' This function clusters cells using the Seurat package. It normalizes the data,
+#' finds variable features, scales the data, runs PCA, finds neighbours, then
+#' clusters the cells across the range of resolutions set by 'res'. The
+#' column 'seurat_clusters' is set according to the first resolution in the
+#' series with a stable ARI value, as indicated in the ARI plot (if 
+#' 'plot_path' is not FALSE). A metadata table with cell names and clusters
+#' is saved in the 'analysis_cache' directory and the path returned.
+#' 
+#' @param seurat_object A Seurat object (or its path) to be clustered.
 #' @param res A vector of resolution values to use for clustering.
 #' @param dims The number of dimensions to use for clustering.
+#' @param regress_out Vector of column names for variables to regress out.
+#' @param do.plot Logical. Save the ARI plot. Default is TRUE.
+#' @param ... Additional arguments to be passed to the function.
 #'
-#' @return A metadata table with the cell names and their corresponding clusters.
+#' @return The path to a metadata table with cell names and clusters in csv format.
 #'
 #' @importFrom Seurat NormalizeData FindVariableFeatures ScaleData RunPCA FindNeighbors FindClusters
 #' @importFrom dplyr select matches
 #' @importFrom tibble rownames_to_column as_tibble
 #'
 #' @examples
+#' if (interactive()) {
 #' data("pbmc_small")
 #' seurat_object <- CreateSeuratObject(counts = pbmc_small)
 #' seurat_cluster(seurat_object)
-#'
+#' }
+#' 
 #' @export
-seurat_cluster_ari <- function(seurat_object, res = seq(0.1, 1.5, by = 0.05), dims = 30, regress_out = NULL, remove_outliers = TRUE, ...) {
+seurat_cluster_ari <- function(
+    seurat_object,
+    res = seq(0.1, 1.5, by = 0.05),
+    dims = 30,
+    regress_out = NULL,
+    plot = TRUE, ...) {
     set.seed(42)
 
     seurat_object <- load_seurat(seurat_object)
-
-    if (isTRUE(remove_outliers)) {
-        if ("is_outlier" %in% colnames(seurat_object[[]])) {
-            seurat_object <- seurat_object[, !seurat_object[["is_outlier"]]]
-        } else {
-            message("No \"is_outlier\" column found in seurat_object, run seurat_flag_outliers_mad() first. Skipping outlier removal.")
-        }
-    }
 
     # Run the standard workflow for visualization and clustering -------------------
     # https://satijalab.org/seurat/articles/pbmc3k_tutorial.html
@@ -39,27 +47,19 @@ seurat_cluster_ari <- function(seurat_object, res = seq(0.1, 1.5, by = 0.05), di
         Seurat::FindNeighbors(reduction = "pca", dims = 1:dims) |>
         Seurat::FindClusters(resolution = res, method = "igraph")
 
-    sample_id <- seurat_object@meta.data$orig.ident[1]
-    clusters_csv_path <- glue::glue("{analysis_cache}/clusters_out/{sample_id}_clusters.csv")
-    dir.create(dirname(clusters_csv_path), showWarnings = FALSE, recursive = TRUE)
-
     clusters_csv <- seurat_object[[]] |>
-        as.data.frame() |>
         tibble::rownames_to_column(var = "cell") |>
         select("cell", "seurat_clusters", contains("SCT"), -c("nCount_SCT", "nFeature_SCT")) |>
-        unique() |>
         tibble::as_tibble()
     
-    resolution_cols <- grep(res_prefix, names(clusters_csv), value = TRUE)
-    resolutions <- gsub(paste0("^", res_prefix, "\\."), "", resolution_cols) |> as.numeric()
+    resolution_cols <- grep("SCT_snn_res", names(clusters_csv), value = TRUE)
+    resolutions <- gsub("SCT_snn_res\\.", "", resolution_cols) |> as.numeric()
 
     ari_values <- numeric(length(resolutions) - 1)
 
     for (i in seq_along(resolutions)[-length(resolutions)]) {
         col1 <- resolution_cols[i]
         col2 <- resolution_cols[i + 1]
-
-        # Ensure both columns exist to calculate ARI
         ari_values[i] <- mclust::adjustedRandIndex(as.integer(clusters_csv[[col1]]), as.integer(clusters_csv[[col2]]))
     }
     # Identify the first resolution with a significant change
@@ -69,10 +69,11 @@ seurat_cluster_ari <- function(seurat_object, res = seq(0.1, 1.5, by = 0.05), di
     }
     stable_res <- resolutions[stable_res_index]
 
-    plot_path <- "{analysis_cache}/clusters_out/{sample_id}_ari_res{stable_res}.png"
-
-    # if plot path is not null, plot ARI values
-    if (!is.null(plot_path)) {
+    sample_id <- seurat_object@meta.data$orig.ident[1]
+    # if TRUE, plot ARI values
+    if ((do.plot)) {
+        plot_path <- glue::glue("{analysis_cache}/clusters_out/{sample_id}_ari_res{stable_res}.png")
+        dir.create(dirname(plot_path), showWarnings = FALSE, recursive = TRUE)
         ari_data <- data.frame(Resolution = resolutions[-length(resolutions)], ARI = ari_values)
         ggplot(ari_data, aes(x = Resolution, y = ARI)) +
         geom_line() +
@@ -85,8 +86,10 @@ seurat_cluster_ari <- function(seurat_object, res = seq(0.1, 1.5, by = 0.05), di
 
     stable_resolution <- glue::glue("SCT_snn_res.{stable_res}")
 
-    clusters_csv$seurat_clusters <- clusters_csv[[stable_resolution]]
+    clusters_csv_path = glue::glue("{analysis_cache}/clusters_out/{sample_id}_clusters.csv")
+    dir.create(dirname(clusters_csv_path), showWarnings = FALSE, recursive = TRUE)
 
+    clusters_csv$seurat_clusters <- clusters_csv[[stable_resolution]]
     clusters_csv |> write.csv(clusters_csv_path, row.names = FALSE, quote = FALSE)
 
     return(clusters_csv_path)
