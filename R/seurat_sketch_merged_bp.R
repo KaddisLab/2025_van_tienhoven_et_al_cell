908b810406c93356717eeaa89747556d94a2f226
Denis O'Meally <omeally@gmail.com>
2024-03-28 13:14:02 -0700

    add messages

--- R/seurat_sketch_merged_bp.R
+++ R/seurat_sketch_merged_bp.R
@@ -11,13 +11,18 @@ seurat_sketch_merged_bp <- function(seurat_object, n_cells = 750) {
 
     options(parallelly.availableCores.methods = "Slurm")
     hprcc::init_multisession()
-
+    message("Loading Seurat object", seurat_object)
     object <- load_seurat(seurat_object)
+    message("Joining layers")
     object <- SeuratObject::JoinLayers(object)
+    #TODO Use scTransform here to regress out unwanted sources of variation?
+    message("Normalizing data")
     object <- Seurat::NormalizeData(object)
+    message("Splitting by sample")
     object[["RNA"]] <- split(object[["RNA"]], f = object$orig.ident)
+    message("Finding variable features")
     object <- Seurat::FindVariableFeatures(object, verbose = FALSE)
-
+    message("Sketching data")
     #  Sample representative cells from each dataset
     object <- Seurat::SketchData(
         object = object,
@@ -26,9 +31,9 @@ seurat_sketch_merged_bp <- function(seurat_object, n_cells = 750) {
         sketched.assay = "sketch")
 
     object_path <- glue::glue("{analysis_cache}/data/merged_seurat_bp_sketch_{n_cells}cells.rds")
-
+    message("Saving object to ", object_path)
     # Save seurat object
     saveRDS(object, file = object_path)
-
+    message("Done")
     return(object_path)
 }
