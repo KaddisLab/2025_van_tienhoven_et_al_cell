ff12b315b239055e68d300bd966725921f0baf73
Denis O'Meally <omeally@gmail.com>
2024-04-02 21:35:54 -0700

    Add seurat_merge_and_sketch function to R file

--- R/seurat_merge_and_sketch.R
+++ R/seurat_merge_and_sketch.R
@@ -3,16 +3,30 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param merged_seurat_path
+#' @param seurat_object(s)
 #' @return
 #' @author Denis O'Meally
 #' @export
-seurat_sketch_merged_bp <- function(seurat_object, n_cells = 750) {
+seurat_merge_and_sketch <- function(seurat_object, n_cells = 750) {
 
     options(parallelly.availableCores.methods = "Slurm")
     hprcc::init_multisession()
-    message("Loading Seurat object", seurat_object)
-    object <- load_seurat(seurat_object)
+
+    if (length(seurat_object) > 1) {
+        message("Loading objects...")
+        object_list<-list()
+        for (object in seurat_object) {
+            object <- load_seurat(object)
+            sample_id <- object[["orig.ident"]][1, ]
+            object_list[[sample_id]] <- object
+            message("Loaded ", sample_id)
+        }
+        message("Merging objects...")
+        object <- scCustomize::Merge_Seurat_List(object_list)
+    } else {
+        message("Loading Seurat object", seurat_object)
+        object <- load_seurat(seurat_object)
+    }
     message("Joining layers")
     object <- SeuratObject::JoinLayers(object)
     #TODO Use scTransform here to regress out unwanted sources of variation?
