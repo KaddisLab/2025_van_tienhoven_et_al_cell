c00fe821ed4acb2ef84aadbfe1141542c3530ef3
Denis O'Meally <omeally@gmail.com>
2024-04-08 19:00:46 -0700

    Add seurat_sketch function for subsampling and sketching Seurat object

---
+++ R/seurat_sketch.R
@@ -0,0 +1,37 @@
+#' Sketch Seurat Object
+#'
+#' Subsamples a Seurat object to a specified number of cells, normalizes the data,
+#' finds variable features, and performs sketching based on leverage scores.
+#' The sketched data is then saved to a specified path.
+#'
+#' @param seurat_object The path to the Seurat object file to be loaded.
+#' @param n_cells The number of cells to subsample to; defaults to 750.
+#' @return The path to the saved sketched Seurat object.
+#' @export
+#' @examples
+#' \dontrun{
+#' if (interactive()) {
+#'     seurat_path <- "path/to/seurat_object.rds"
+#'     sketched_path <- seurat_sketch(seurat_path, n_cells = 500)
+#' }
+#' }
+#' @importFrom Seurat NormalizeData FindVariableFeatures
+#' @importFrom qs qsave
+#' @importFrom glue glue
+seurat_sketch <- function(seurat_object, n_cells = 750) {
+    object <- load_seurat(seurat_object)
+    sample_id <- object[["orig.ident"]][1, ]
+    object <- Seurat::NormalizeData(object)
+    object <- Seurat::FindVariableFeatures(object, verbose = FALSE)
+    #  Sample representative cells from each dataset
+    object <- Seurat::SketchData(
+        object = object,
+        ncells = n_cells,
+        method = "LeverageScore",
+        sketched.assay = "sketch"
+    )
+    object_path <- glue::glue("{analysis_cache}/seurat_sketch_out/{sample_id}_{n_cells}_cells.qs")
+    dir.create(dirname(object_path), showWarnings = FALSE, recursive = TRUE)
+    qs::qsave(object, file = object_path)
+    return(object_path)
+}
