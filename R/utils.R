bf758064b79255e7b4dee8ebb61eea70cdcd4a44
Denis O'Meally <omeally@gmail.com>
2024-06-12 15:11:31 -0700

    refactor: Add support for saving objects in different file formats

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,16 @@ save_results<-function(object, object_path) {
       # Save results
     dir.create(dirname(object_path), showWarnings = FALSE, recursive = TRUE)
     message("Saving object...")
+    file_type <- tools::file_ext(object_path)
+    if (file_type == "csv") {
+        readr::write_csv(object, object_path)
+    } else if (file_type == "rds") {
+        saveRDS(object, object_path)
+    } else if (file_type == "qs") {
+        qs::qsave(object, file = object_path)
+    } else {
+        stop("Unsupported file type: ", file_type)
+    }
     qs::qsave(object, file = object_path)
     message("Done.")
     return(object_path)
