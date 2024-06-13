5a8045fc4c19717266290921aa04869b7400410b
Denis O'Meally <omeally@gmail.com>
2024-06-12 20:38:15 -0700

    refactor: Improve seurat_aggregate_cell_annot function to handle different input types

--- R/seurat_aggregate_cell_annot.R
+++ R/seurat_aggregate_cell_annot.R
@@ -1,33 +1,42 @@
 seurat_aggregate_cell_annot <- function(...) {
-    # Collect all input arguments as a list of vectors
+    # Collect all input arguments as a list
     paths_list <- list(...)
 
     # Initialize an empty list to store each dataframe
     df_list <- list()
 
-    # Process each vector of file paths in the list
-    for (paths in paths_list) {
-        # Modify paths to end with .csv
-        csv_paths <- sapply(paths, function(path) {
-            sub("\\.[^\\.]*$", ".csv", path)
-        })
+    # Process each element in the list
+    for (element in paths_list) {
+        if (is.character(element)) {
+            # If the element is a character vector (paths), read CSV files into a dataframe
+            csv_paths <- sapply(element, function(path) {
+                sub("\\.[^\\.]*$", ".csv", path)
+            })
 
-        # Read data into a dataframe and handle errors
-        df <- tryCatch(
-            {
-                readr::read_csv(file = csv_paths)
-            },
-            error = function(e) {
-                warning("Error reading CSV files: ", e$message)
-                NULL # Return NULL for any read errors to filter out later
+            df <- tryCatch(
+                {
+                    readr::read_csv(file = csv_paths)
+                },
+                error = function(e) {
+                    warning("Error reading CSV files: ", e$message)
+                    NULL # Return NULL for any read errors to filter out later
+                }
+            )
+
+            # Only add the dataframe to the list if it's not NULL
+            if (!is.null(df)) {
+                df_list[[length(df_list) + 1]] <- df
             }
-        )
-
-        # Only add the dataframe to the list if it's not NULL
-        if (!is.null(df)) {
-            df_list[[length(df_list) + 1]] <- df
+        } else if (is.data.frame(element)) {
+            # If the element is already a dataframe, add it to the list
+            df_list[[length(df_list) + 1]] <- element
+        } else {
+            warning("Unsupported element type: ", class(element))
         }
     }
+
+    # Filter out any NULL dataframes (in case of read errors)
+    df_list <- Filter(Negate(is.null), df_list)
 
     # Check if 'cell' column exists in all dataframes
     if (!all(sapply(df_list, function(df) "cell" %in% names(df)))) {
